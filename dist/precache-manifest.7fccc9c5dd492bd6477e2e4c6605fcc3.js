77bbf1f76254bfe7d61dd9cbb504cf06efc49ffe
wenjinzhuo <1298323857@qq.com>
2020-08-30 21:46:30 +0800

    f c

---
+++ dist/precache-manifest.7fccc9c5dd492bd6477e2e4c6605fcc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd03f16517b43eb69968",
+    "url": "/css/app.37013037.css"
+  },
+  {
+    "revision": "1bef9afbc0937539726a6594efaf5164",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6e07cc669e81355ba8c",
+    "url": "/js/about.33e24808.js"
+  },
+  {
+    "revision": "bd03f16517b43eb69968",
+    "url": "/js/app.884013d3.js"
+  },
+  {
+    "revision": "7522c0720069a6f47515",
+    "url": "/js/chunk-vendors.ce061d87.js"
+  },
+  {
+    "revision": "c58f7bb895e7e53f1336881cc19ff5ea",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
